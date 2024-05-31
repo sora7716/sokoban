550681d7bb89cb98f7f4139948e401428ec39a9f
sora <147121795+sora7716@users.noreply.github.com>
2024-06-01 01:56:53 +0900

    タイトルとゲームクリアーのフォントを表示できるようになりました。

--- Assets/TitleFontManager.cs
+++ Assets/TitleFontManager.cs
@@ -2,19 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 public class TitleFontManager : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI titleFont;
 
-    IEnumerator FadeIn()
+    public IEnumerator FadeIn()
     {
-        titleFont.text = "sokoban";
+        string sceneName = SceneManager.GetActiveScene().name;
+        if (sceneName == "Title")  // シーン1の名前を確認します
+        {
+            titleFont.text = "Sokoban";
+        }
+        else
+        {
+            titleFont.text = "GameClear";
+        }
         while (true)
         {
             for (int i = 0; i < 255; i++)
             {
-                titleFont.color = titleFont.color + new Color32(0, 0, 0, 1);
+                titleFont.color = titleFont.color + new Color(0, 0, 0, 1.0f/255);
                 yield return new WaitForSeconds(0.01f);
             }
         }
@@ -22,12 +31,22 @@ public class TitleFontManager : MonoBehaviour
 
     void Start()
     {
+        string sceneName = SceneManager.GetActiveScene().name;
         titleFont.text = "";
-        titleFont.color = new Color(0.0f, 0.0f, 0.0f, 0.0f);
-        StartCoroutine("FadeIn");
+        if (sceneName == "Title")  // シーン1の名前を確認します
+        {
+            titleFont.color = new Color(1.0f, 0.0f, 0.0f, 0.0f);
+        }
+        else
+        {
+            titleFont.color = new Color(0.0f, 1.0f, 0.0f, 0.0f);
+        }
+       
+       StartCoroutine("FadeIn");
     }
 
-   
+    
+
     void Update()
     {
         
